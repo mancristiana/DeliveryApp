03d19f8efe24ced96520dbbbada5c7739ed1012a
Daniel Constantin <danielconstantin13@gmail.com>
2015-06-19 18:48:42 +0200

    Added css

--- src/dk/kea/swc/cadd/delivery/view/LocationOverviewController.java
+++ src/dk/kea/swc/cadd/delivery/view/LocationOverviewController.java
@@ -7,12 +7,14 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -53,10 +55,9 @@ public class LocationOverviewController {
 		locationTable.setItems(locationDAO.getLocations());
 			
 		// Resize the columns (with percentages) when the window is enlarged //TODO COPYRIGHT
-		cityNameColumn		.prefWidthProperty().bind(locationTable.widthProperty().multiply(0.296));
-		storageNameColumn	.prefWidthProperty().bind(locationTable.widthProperty().multiply(0.30));
-		priceColumn			.prefWidthProperty().bind(locationTable.widthProperty().multiply(0.25));
-		editColumn			.prefWidthProperty().bind(locationTable.widthProperty().multiply(0.15));
+		cityNameColumn		.prefWidthProperty().bind(locationTable.widthProperty().subtract(70).multiply(0.34));
+		storageNameColumn	.prefWidthProperty().bind(locationTable.widthProperty().subtract(70).multiply(0.33));
+		priceColumn			.prefWidthProperty().bind(locationTable.widthProperty().subtract(70).multiply(0.33));
 		
 		// Initialize the table with the four columns
 		cityNameColumn		.setCellValueFactory(cellData -> cellData.getValue().cityNameProperty());
@@ -72,29 +73,32 @@ public class LocationOverviewController {
 	 * A table cell containing a button for editing a location. 
 	 */
     private class AddEditCell extends TableCell<Location, Boolean> {
-      final Button button = new Button("Edit");
-
-      AddEditCell() {
-    	  button.setOnAction(new EventHandler<ActionEvent>() {
-          @Override public void handle(ActionEvent actionEvent) {
-        	  showLocationEditDialog(locationTable.getItems().get(getTableRow().getIndex()));
-          }
-        });
-      }
+    	final Button button = new Button();
+    	HBox wrap = new HBox();
+    	
+	    AddEditCell() {
+	    	button.setId("edit-button");
+	    	button.setOnAction(new EventHandler<ActionEvent>() {
+	    		@Override public void handle(ActionEvent actionEvent) {
+	        		showLocationEditDialog(locationTable.getItems().get(getTableRow().getIndex()));
+	    		}
+	        });
+	    	wrap.setAlignment(Pos.CENTER);
+	    	wrap.getChildren().add(button);
+	      }
       
-      /** 
-       * Places an edit button in the row only if the row is not empty. 
-       */
-      @Override protected void updateItem(Boolean item, boolean empty) {
-        super.updateItem(item, empty);
-        if (!empty) {
-          setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-          setGraphic(button);
-        } else {
-          setGraphic(null);
-        }
-      }
-      
+	    /** 
+	     * Places an edit button in the row only if the row is not empty. 
+	     */
+	    @Override protected void updateItem(Boolean item, boolean empty) {
+	        super.updateItem(item, empty);
+	        if (!empty) {
+	          setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+	          setGraphic(wrap);
+	        } else {
+	          setGraphic(null);
+	        }
+	    }
     }
     
     /**
