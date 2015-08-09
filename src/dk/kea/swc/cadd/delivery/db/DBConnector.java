5b17affdb10bd8620d5dcc62c0f089ff35511f69
Daniel Constantin <danielconstantin13@gmail.com>
2015-08-09 17:48:58 +0200

    some minor changes

--- src/dk/kea/swc/cadd/delivery/db/DBConnector.java
+++ src/dk/kea/swc/cadd/delivery/db/DBConnector.java
@@ -6,30 +6,13 @@ import java.sql.SQLException;
 
 public class DBConnector {
     
-    private static final String USER = "cadd";
-    private static final String PASS = "teamrocket";
-    private static final String DB = "cadd";
-    private static final String URL = "jdbc:mysql://138.128.216.12";
-    private static final String PORT = "3306";
-    
-    public static Connection getConnection() {
-        Connection con = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            String urlForConn = URL + ":" + PORT + "/" + DB;
-            con = DriverManager.getConnection(urlForConn, USER, PASS);
-        } catch (ClassNotFoundException | SQLException e) {
-        	e.printStackTrace();
-        }
-        return con;
-    }
-    
+//    private static final String USER = "cadd";
+//    private static final String PASS = "teamrocket";
+//    private static final String DB = "cadd";
+//    private static final String URL = "jdbc:mysql://138.128.216.12";
+//    private static final String PORT = "3306";
+//    
 //    public static Connection getConnection() {
-//    	String USER = "root";
-//    	String PASS = "";
-//    	String DB = "cadd";
-//    	String URL = "jdbc:mysql://localhost";
-//    	String PORT = "3306";
 //        Connection con = null;
 //        try {
 //            Class.forName("com.mysql.jdbc.Driver");
@@ -40,5 +23,22 @@ public class DBConnector {
 //        }
 //        return con;
 //    }
+    
+    public static Connection getConnection() {
+    	String USER = "root";
+    	String PASS = "";
+    	String DB = "cadd";
+    	String URL = "jdbc:mysql://localhost";
+    	String PORT = "3306";
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String urlForConn = URL + ":" + PORT + "/" + DB;
+            con = DriverManager.getConnection(urlForConn, USER, PASS);
+        } catch (ClassNotFoundException | SQLException e) {
+        	e.printStackTrace();
+        }
+        return con;
+    }
     
 }
